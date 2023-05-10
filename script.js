0bbf4dfc98ccb35f6b8eaddf1e38593b2a6a5591
Tyler Vick <jrtvick@gmail.com>
2023-05-10 16:49:42 -0400

    got the clock running on the header

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
+var currentHour = new Date().getHours()
+
+var currentDayEl = document.getElementById('currentDay')
+currentDayEl.textContent = new Date().toLocaleString('en-US', {
+  month: 'long',
+  day: 'numeric',
+  year: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  second: 'numeric',
+})
+
+setInterval(()=>{
+  currentDayEl.textContent = new Date().toLocaleString('en-US', {
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  })
+}, 1000)
 
 var rowContainer = document.getElementById("rowContainer")
 
@@ -6,7 +28,7 @@ for (var i = 0; i < hours.length; i++) {
 
   var outerDiv = document.createElement('div')
   outerDiv.id = hours[i]
-  outerDiv.classList.add('row', 'time-block')
+  outerDiv.classList.add('row', 'time-block', getColor(hours[i]))
   outerDiv.innerHTML = `<div class="col-2 col-md-1 hour text-center py-3">${getHour(hours[i])}</div>
   <textarea class="col-8 col-md-10 description" rows="3">${localStorage.getItem(hours[i]) || ''}</textarea>
   <button id="${hours[i]}" class="btn saveBtn col-2 col-md-1" aria-label="save">
@@ -17,9 +39,9 @@ for (var i = 0; i < hours.length; i++) {
 }
 
 var saveButtons = document.querySelectorAll(".saveBtn")
-console.log(saveButtons)
+
 var buttonsArray = Array.from(saveButtons)
-console.log(buttonsArray)
+
 
 
 function getHour(hour) {
@@ -51,4 +73,10 @@ for (var i = 0; i < buttonsArray.length; i++) {
     var valueToStore = (e.currentTarget.previousElementSibling.value)
     localStorage.setItem(e.currentTarget.id, valueToStore)
   })
+}
+
+function getColor(hour){
+  if (hour < currentHour) return 'past';
+  else if (hour > currentHour) return 'future';
+  else return 'present'
 }
