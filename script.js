28d4fc18b27af055b7cb9f7cfb5a104facd21304
Tyler Vick <jrtvick@gmail.com>
2023-05-10 16:08:59 -0400

    created a function to display hours in am and pm

--- script.js
+++ script.js
@@ -3,15 +3,41 @@ var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 var rowContainer = document.getElementById("rowContainer")
 
 for (var i = 0; i < hours.length; i++) {
-  console.log(hours[i])
+
   var outerDiv = document.createElement('div')
   outerDiv.id = hours[i]
   outerDiv.classList.add('row', 'time-block')
-  outerDiv.innerHTML = `<div class="col-2 col-md-1 hour text-center py-3">9AM</div>
+  outerDiv.innerHTML = `<div class="col-2 col-md-1 hour text-center py-3">${getHour(hours[i])}</div>
   <textarea class="col-8 col-md-10 description" rows="3"> </textarea>
   <button class="btn saveBtn col-2 col-md-1" aria-label="save">
     <i class="fas fa-save" aria-hidden="true"></i>
   </button>`
 
   rowContainer.append(outerDiv);
+}
+
+var saveButtons = document.querySelectorAll(".saveBtn")
+console.log(saveButtons)
+
+function getHour(hour) {
+  switch (hour) {
+    case "09":
+      return "9AM"
+    case "10":
+      return "10AM"
+    case "11":
+      return "11AM"
+    case "12":
+      return "12PM"
+    case "13":
+      return "1PM"
+    case "14":
+      return "2PM"
+    case "15":
+      return "3PM"
+    case "16":
+      return "4PM"
+    case "17":
+      return "5PM"
+  }
 }
