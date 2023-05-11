52dd3e0bb559785501845ad42325214c3e989099
Tyler Vick <jrtvick@gmail.com>
2023-05-10 20:34:40 -0400

    finished my rough draft for the night

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 var currentHour = new Date().getHours();
 
+// Here I created a function to display a live clock at the top of the page using dayjs and an if statement.
 function setTime() {
   var today = dayjs();
   var stopTimerFlag = false;
@@ -22,7 +23,7 @@ setTime();
 // Created a global variable to call my div tags that house the schedule on the webpage.
 var rowContainer = document.getElementById("rowContainer");
 
-// 
+// This is a for loop that was created to display each hour of the schedule. The idea is to draw from the HTML to repeat the single row of the table nine times. 
 for (var i = 0; i < hours.length; i++) {
   var outerDiv = document.createElement('div');
   outerDiv.id = hours[i];
@@ -32,10 +33,11 @@ for (var i = 0; i < hours.length; i++) {
   rowContainer.append(outerDiv);
 }
 
-
+// More variables made for my buttons to be individual rather than all share the same storage.
 var saveButtons = document.querySelectorAll(".saveBtn");
 var buttonsArray = Array.from(saveButtons);
 
+// This switch function allows me to draw from my hours array at the top of my code and display the values in a way that makes the most sense for the user to schedule their tasks.
 function getHour(hour) {
   switch (hour) {
     case "09":
@@ -59,14 +61,16 @@ function getHour(hour) {
   }
 };
 
+// Made a for loop with the help of my tutor to add an event listener to all of the buttons for each hour. I was running into an issue of part of the button being clicked storing the value to local storage but if I did not click on the small floppy disk image, it wouldn't save.
 for (var i = 0; i < buttonsArray.length; i++) {
-  buttonsArray[i].addEventListener("click", function(event){
+  buttonsArray[i].addEventListener("click", function(event) {
     var valueToStore = (event.currentTarget.previousElementSibling.value);
     localStorage.setItem(event.currentTarget.id, valueToStore);
   })
 }
 
-function getColor(hour){
+// This function draws the appropriate colours from the style sheet. The schedule will visually display differently depending on time of day, giving the user an understanding of when to schedule something for.
+function getColor(hour) {
   if (hour < currentHour) return 'past';
   else if (hour > currentHour) return 'future';
   else return 'present';
